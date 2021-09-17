b55b290516b2f46a58412a0d9ecbebf4ab201af8
Vaishak J Nair <83900782+Vaishak-JN@users.noreply.github.com>
2021-09-17 23:21:53 +0530

    V5

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 //Method 1 
 
-let t=document.getElementsByClassName("container")
-let count=10;
+// let t=document.getElementsByClassName("container")
+// let count=10;
 
-function countdown(num,callback){
-    if(num>0){
-        setTimeout(() => {
-            callback(num)
+// function countdown(num,callback){
+//     if(num>0){
+//         setTimeout(() => {
+//             callback(num)
             
-        }, 1000);
-        t[0].innerText=num;
-    }else{
-        t[0].innerText="Wish you a good day"
-    }
+//         }, 1000);
+//         t[0].innerText=num;
+//     }else{
+//         t[0].innerText="Wish you a good day"
+//     }
     
-}
-function callback(num){
-        num=num-1;
-        countdown(num,callback)
-}
+// }
+// function callback(num){
+//         num=num-1;
+//         countdown(num,callback)
+// }
 
-window.onload=setTimeout(() => {
-    countdown(count,callback)
-}, 1000);
+// window.onload=setTimeout(() => {
+//     countdown(count,callback)
+// }, 1000);
 
 // Method 2
 
@@ -71,43 +71,61 @@ window.onload=setTimeout(() => {
 //     }, 1000);
 // })
 
-// Method 4
 
-// let p=document.getElementsByClassName("container")
-// function countDown(){
-//     setTimeout(() => {
-//         p[0].innerText="10"
-//         setTimeout(() => {
-//             p[0].innerText="9"
-//             setTimeout(() => {
-//                 p[0].innerText="8";
-//                 setTimeout(() => {
-//                     p[0].innerText="7";
-//                     setTimeout(() => {
-//                         p[0].innerText="6";
-//                         setTimeout(() => {
-//                             p[0].innerText="5";
-//                             setTimeout(() => {
-//                                 p[0].innerText="4";
-//                                 setTimeout(() => {
-//                                     p[0].innerText="3";
-//                                     setTimeout(() => {
-//                                         p[0].innerText="2";
-//                                         setTimeout(() => {
-//                                             p[0].innerText="1";
-//                                             setTimeout(() => {
-//                                                 p[0].innerText="Wish you a good day";
-//                                             }, 1000);
-//                                         }, 1000);
-//                                     }, 1000);
-//                                 }, 1000);
-//                             }, 1000);
-//                         }, 1000);
-//                     }, 1000);
-//                 }, 1000);
-//             }, 1000);
-//         }, 1000);
+// Method 4 (using just setInerval)
+
+// let time=document.getElementsByClassName("container")
+// let count=11;
+// function timer(count){
+//     setInterval(() => {
+//         count=count-1
+//         if(count>0){           
+//             time[0].innerText=count
+//         }else{           
+//             time[0].innerText="Wish you a good day";
+//         }
 //     }, 1000);
 // }
+// timer(count) 
 
-// window.onload=countDown
+
+// Method 5
+
+let p=document.getElementsByClassName("container")
+function countDown(){
+    setTimeout(() => {
+        p[0].innerText="10"
+        setTimeout(() => {
+            p[0].innerText="9"
+            setTimeout(() => {
+                p[0].innerText="8";
+                setTimeout(() => {
+                    p[0].innerText="7";
+                    setTimeout(() => {
+                        p[0].innerText="6";
+                        setTimeout(() => {
+                            p[0].innerText="5";
+                            setTimeout(() => {
+                                p[0].innerText="4";
+                                setTimeout(() => {
+                                    p[0].innerText="3";
+                                    setTimeout(() => {
+                                        p[0].innerText="2";
+                                        setTimeout(() => {
+                                            p[0].innerText="1";
+                                            setTimeout(() => {
+                                                p[0].innerText="Wish you a good day";
+                                            }, 1000);
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}
+
+window.onload=countDown
